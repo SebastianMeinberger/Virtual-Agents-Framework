797dda1131a1343427a5f87676f40dce010c232d
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2025-05-30 13:08:41 +0200

    Renamed sample scene and moved some too specific scipts into it

---
+++ Assets/Virtual Agents Framework/Runtime/Scripts/UI Manager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace i5.VirtualAgents
+{
+    /// <summary>
+    /// Used by the Agent UI Tasks to reference TMP text and input components.
+    /// </summary>
+    public class UIManager : MonoBehaviour
+    {
+        public GameObject agentText;
+        public GameObject userInput;
+    }
+}
