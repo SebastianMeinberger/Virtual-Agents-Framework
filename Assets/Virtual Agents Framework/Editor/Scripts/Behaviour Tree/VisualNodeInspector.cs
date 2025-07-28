764ec70b0e1ac5aea26815307f171c49c9cec7ad
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2025-07-28 12:09:22 +0200

    Property fields are now created manually inorder to enable ValueChangeCallbacks

--- Assets/Virtual Agents Framework/Editor/Scripts/Behaviour Tree/VisualNodeInspector.cs
+++ Assets/Virtual Agents Framework/Editor/Scripts/Behaviour Tree/VisualNodeInspector.cs
@@ -1,11 +1,9 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEditor;
 using i5.VirtualAgents.BehaviourTrees.Visual;
 using i5.VirtualAgents.AgentTasks;
-using System;
-using Unity.Properties;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
 
 namespace i5.VirtualAgents.Editor.BehaviourTrees
 {
@@ -15,25 +13,71 @@ namespace i5.VirtualAgents.Editor.BehaviourTrees
     [CustomEditor(typeof(VisualNode))]
     public class VisualNodeInspector : UnityEditor.Editor
     {
+        VisualElement inspector;
+        List<PropertyField> fields = new();
 
-        // Creates a property field of the provided type for the serialized data saved in the array with the name propertyName
-        private int CreatePropertyField(SerializableType type, int counter)
+        private void OnTreeChange(BehaviourTreeAsset tree, VisualElement element, VisualNode targetNode, SerializedObject serializedObject)
         {
-            VisualNode targetNode = target as VisualNode;
-            // Retrieve the serialized array
-            string propertyPath = SerializationDataContainer.TypeToPath(type);
-            SerializedProperty baseProperty = serializedObject.FindProperty("Data." + propertyPath + ".data");
-            // Create the property field for the element with index counter
-            EditorGUILayout.PropertyField(baseProperty.GetArrayElementAtIndex(counter).FindPropertyRelative("Value"), new GUIContent(targetNode.Data.GetKeyByIndex(counter, type)));
-            return 0;
+            // TODO Clear old data
+            
+            targetNode.GetCopyOfSerializedInterface().Serialize(targetNode.Data);
+            /*
+            foreach (string key in targetNode.Data.GetKeysInSerializationOrder())
+            {
+                targetNode.exposeToLLM.Add(key, false);
+            }
+            */
+            serializedObject.Update();
+            
         }
-        public override void OnInspectorGUI()
+
+
+        public void OnInspectorGUI(VisualElement element)
         {
             VisualNode targetNode = target as VisualNode;
-            if(!targetNode.CheckIntegrity())
+            bool isBehaviorTreeTask = targetNode.DeserializeType() is BehaviourTreeTask behaviourTreeTask;
+            int counter = 0;
+            // Creates a property field of the provided type for the serialized data saved in the array with the name propertyName
+            int CreatePropertyField(SerializableType type, int index)
+            {
+                VisualNode targetNode = target as VisualNode;
+                // Retrieve the serialized array
+                
+                string propertyPath = SerializationDataContainer.TypeToPath(type);
+                SerializedProperty baseProperty = serializedObject.FindProperty("Data." + propertyPath + ".data");
+                PropertyField field = new(baseProperty.GetArrayElementAtIndex(index).FindPropertyRelative("Value"));
+                string key = targetNode.Data.GetKeyByIndex(index, type);
+                field.label = key;
+                field.BindProperty(serializedObject);
+                element.Add(field);
+                
+
+                if (isBehaviorTreeTask)
+                {
+                    if (type == SerializableType.TREE && index == 0)
+                    {
+                        field.RegisterValueChangeCallback((x) => OnTreeChange(x.changedProperty.objectReferenceValue as BehaviourTreeAsset, element, targetNode, serializedObject));
+                    }
+                    else
+                    {
+                        //int boolIndex = 0;
+                        //for (; targetNode.exposeToLLM.data[boolIndex].Key != key && boolIndex < targetNode.exposeToLLM.data.Count; boolIndex++) { }
+                        SerializedProperty boolPropery = serializedObject.FindProperty("Data.exposeToLLM").GetArrayElementAtIndex(counter);
+                        PropertyField fieldExposeToLLM = new(boolPropery);
+                        fieldExposeToLLM.label = "LLM Function";
+                        fieldExposeToLLM.BindProperty(serializedObject);
+                        element.Add(fieldExposeToLLM);
+                    }
+                }
+                counter++;
+                return 0;
+            }
+
+            if (!(targetNode.GetCopyOfSerializedInterface() is BehaviourTreeTask) && !targetNode.CheckIntegrity())
             {
                 targetNode.ReSerialize();
             }
+
             targetNode.Data.MapOverData(CreatePropertyField);
             serializedObject.ApplyModifiedProperties();
         }
